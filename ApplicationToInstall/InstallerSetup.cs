cf461294ecb51195b250943ce859054582537dac
André de Mattos Ferraz <fandre@gmail.com>
2015-07-14 17:32:11 -0300

    First Commit
    First Commit

---
+++ ApplicationToInstall/InstallerSetup.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ApplicationToInstall
+{
+    [RunInstaller(true)]
+    public partial class InstallerSetup : System.Configuration.Install.Installer
+    {
+        public InstallerSetup()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+
+            try
+            {
+                AddConfigurationFileDetails();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Falha ao atualizar o arquivo de configuração da aplicação: " + e.Message);
+                base.Rollback(savedState);
+            }
+        }
+
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+
+        private void showParameters()
+        {
+            StringBuilder sb = new StringBuilder();
+            StringDictionary myStringDictionary = this.Context.Parameters;
+            if (this.Context.Parameters.Count > 0)
+            {
+                foreach (string myString in this.Context.Parameters.Keys)
+                {
+                    sb.AppendFormat("String={0} Value= {1}\n", myString,
+                    this.Context.Parameters[myString]);
+                }
+            }
+            MessageBox.Show(sb.ToString());
+        }
+
+        private void AddConfigurationFileDetails()
+        {
+            try
+            {
+                string TESTPARAMETER = Context.Parameters["TESTPARAMETER"];
+
+                // Get the path to the executable file that is being installed on the target computer
+                string assemblypath = Context.Parameters["assemblypath"];
+                string appConfigPath = assemblypath + ".config";
+
+                // Write the path to the app.config file
+                XmlDocument doc = new XmlDocument();
+                doc.Load(appConfigPath);
+
+                XmlNode configuration = null;
+                foreach (XmlNode node in doc.ChildNodes)
+                    if (node.Name == "configuration")
+                        configuration = node;
+
+                if (configuration != null)
+                {
+                    //MessageBox.Show("configuration != null");
+                    // Get the ‘appSettings’ node
+                    XmlNode settingNode = null;
+                    foreach (XmlNode node in configuration.ChildNodes)
+                    {
+                        if (node.Name == "appSettings")
+                            settingNode = node;
+                    }
+
+                    if (settingNode != null)
+                    {
+                        //MessageBox.Show("settingNode != null");
+                        //Reassign values in the config file
+                        foreach (XmlNode node in settingNode.ChildNodes)
+                        {
+                            //MessageBox.Show("node.Value = " + node.Value);
+                            if (node.Attributes == null)
+                                continue;
+                            XmlAttribute attribute = node.Attributes["value"];
+                            //MessageBox.Show("attribute != null ");
+                            //MessageBox.Show("node.Attributes['value'] = " + node.Attributes["value"].Value);
+                            if (node.Attributes["key"] != null)
+                            {
+                                //MessageBox.Show("node.Attributes['key'] != null ");
+                                //MessageBox.Show("node.Attributes['key'] = " + node.Attributes["key"].Value);
+                                switch (node.Attributes["key"].Value)
+                                {
+                                    case "TestParameter":
+                                        attribute.Value = TESTPARAMETER;
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                    doc.Save(appConfigPath);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
